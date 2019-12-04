358924b5f7074f1f519114d51a1b63f7e29d940c
SurajGupta07 <surajbg07@gmail.com>
2019-12-04 23:41:20 +0530

    initial commit

---
+++ config.bak.php
@@ -0,0 +1,36 @@
+<?php
+	$dbServer = 'localhost';
+	$dbUsername = 'root';
+	$dbPassword = '';
+	$dbDatabase = 'gpms_db';
+
+	$adminConfig = array(
+		'adminUsername' => "admin",
+		'adminPassword' => "827ccb0eea8a706c4c34a16891f84e7b",
+		'notifyAdminNewMembers' => "0",
+		'defaultSignUp' => "1",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "Full Name",
+		'custom2' => "Address",
+		'custom3' => "City",
+		'custom4' => "State",
+		'MySQLDateFormat' => "%d/%m/%Y",
+		'PHPDateFormat' => "j/n/Y",
+		'PHPDateTimeFormat' => "d/m/Y, h:i a",
+		'senderName' => "Membership management",
+		'senderEmail' => "surajbg07@gmail.com",
+		'approvalSubject' => "Your membership is now approved",
+		'approvalMessage' => "Dear member,\r\n\r\nYour membership is now approved by the admin. You can log in to your account here:\r\nhttp://localhost/gpmsproject\r\n\r\nRegards,\r\nAdmin",
+		'hide_twitter_feed' => "",
+		'maintenance_mode_message' => "<b>Our website is currently down for maintenance</b><br>\r\nWe expect to be back in a couple hours. Thanks for your patience.",
+		'mail_function' => "mail",
+		'smtp_server' => "ssl://smtp.gmail.com",
+		'smtp_encryption' => "ssl",
+		'smtp_port' => "456",
+		'smtp_user' => "surajbg07@gmail.com",
+		'smtp_pass' => "Govindsunith"
+	);
